796eb55e0b55be5a0eeb72f53f8cda9ca670e2a0
JomBarce <89145244+JomBarce@users.noreply.github.com>
2025-12-15 19:19:47 +0800

    Updated config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+const BACKEND_PORT = Number(process.env.PORT) || 9001;
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 9000,
+    proxy: {
+      '/api': {
+        target: `http://localhost:${BACKEND_PORT}`,
+        changeOrigin: true,
+      },
+    },
+  },
+});
