9503b8a12d0f3787e6b4a24a514ad3b5d5cced01
JomBarce <89145244+JomBarce@users.noreply.github.com>
2026-01-03 23:03:01 +0800

    Updated main.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+mod db;
+mod routes;
+
+use actix_web::{web, App, HttpServer};
+// use actix_cors::Cors;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    config::init();
+    let db = db::mongo::connect().await;
+
+    println!("Server running on 127.0.0.1:9001");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(db.clone()))
+            .service(
+                web::scope("/api")
+                    .route("/hello", web::get().to(routes::hello::hello))
+                    .route("/status", web::get().to(routes::status::status))
+            )
+    })
+    .bind(("127.0.0.1", 9001))?
+    .run()
+    .await
 }
